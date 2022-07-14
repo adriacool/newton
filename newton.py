4e811537892d198aa408895b0ceb72437e116808
Medium Size <adriafrance@my.unt.edu>
2022-07-13 23:24:25 -0500

    Fix typos

--- newton.py
+++ newton.py
@@ -1,8 +1,8 @@
 from sympy import *
+x = Symbol('x') 
 
 #Takes a function and list of values, approximates roots using Newton's method
-def newton(f,x_o)
-  x = Symbol('x')
+def newton(f,x_o):
   df = f.diff(x)
   d_o=lambdify(x,df)
 
@@ -23,5 +23,5 @@ def newton(f,x_o)
     root+=1
     print(f"{root}) Computed x={x_n} in {n} iterations.")
     
-    
-newton(4*x**4-4*x**2,[21**(1/2)/7+.05, 21**(1/2)/7-.001, 21**(1/2)/7+.1])  
+   
+newton(4*x**4-4*x**2,[21**(1/2)/7+.05, 21**(1/2)/7-.001, 21**(1/2)/7+.1])    
