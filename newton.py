67af55995fdc37915819221f18f4d90642fba00a
Medium Size <adriafrance@my.unt.edu>
2022-07-13 21:03:00 -0500

    Add newton.py

---
+++ newton.py
@@ -0,0 +1,27 @@
+from sympy import *
+
+#Takes a function and list of values, approximates roots using Newton's method
+def newton(f,x_o)
+  x = Symbol('x')
+  df = f.diff(x)
+  d_o=lambdify(x,df)
+
+  root=0
+  for x_m in x_o:
+    if d_o(x_m)==0:
+      df+=.1
+    u=f/df
+    u_o=lambdify(x,u)
+
+    n=1
+    while n<2000:
+      x_n=x_m-u_o(x_m)
+      if x_n==x_m:
+        break   
+      x_m=x_n  
+      n+=1
+    root+=1
+    print(f"{root}) Computed x={x_n} in {n} iterations.")
+    
+    
+newton(4*x**4-4*x**2,[21**(1/2)/7+.05, 21**(1/2)/7-.001, 21**(1/2)/7+.1])  
